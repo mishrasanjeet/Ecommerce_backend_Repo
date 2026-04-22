8da888ef1514ea1d847fa3b0e91e37f6c6f51eec
kumarsanjeetmishra8000@gmail.com <kumarsanjeetmishra8000@gmail.com>
2026-04-22 23:08:06 +0530

    Create the parent project Ecommerce.

---
+++ ecommerce-project/src/test/java/ecommerce/microservice/backend/ecommerce/project/EcommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ecommerce.microservice.backend.ecommerce.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
